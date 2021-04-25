4cf8e8c092d02d55fa2ec3abd75012197196e8b8
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2021-04-25 00:38:40 -0500

    version aaaaaaa

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mwrogue",
-    version="0.0.2",
+    version="0.0.3",
     author="RheingoldRiver",
     author_email="river.esports@gmail.com",
     description="Client for accessing Fandom/Gamepedia Esports Wikis",
