111cab3e2e61adae011cb0df9c804e5cde11d535
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2021-05-22 02:54:08 -0500

    Get rid of logs objects, those are in mwcleric

--- mwrogue/logs/move_log_entry.py
+++
@@ -1,9 +0,0 @@
-from mwrogue.logs.log_entry import LogEntry
-from mwrogue.wiki_client import WikiClient
-
-
-class MoveLogEntry(LogEntry):
-    def __init__(self, log, site: WikiClient):
-        super().__init__(log, site)
-        self.title = log['params']['target_title']
-        self.page = site.client.pages[self.title]
