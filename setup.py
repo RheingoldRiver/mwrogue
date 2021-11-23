6259d823aa1fdb9edb0db8ff37e43300906aad63
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2021-11-23 11:23:50 -0600

    is install_requires breaking git install?

--- setup.py
+++ setup.py
@@ -14,5 +14,5 @@ setuptools.setup(
     url="https://github.com/RheingoldRiver/mwrogue",
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
-    install_requires=['mwparserfromhell', 'pytz', 'mwclient>=0.10.1', 'python-dateutil', 'Unidecode', 'mwcleric>=0.6.2']
+    install_requires=['mwparserfromhell', 'pytz', 'mwclient', 'python-dateutil', 'Unidecode', 'mwcleric']
 )
